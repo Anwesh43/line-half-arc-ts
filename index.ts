ac2741181317233cf389d90613f6af7d66dbe38b
Anwesh43 <anweshthecool0@gmail.com>
2020-10-10 17:54:54 +0530

    adding HaflNode class

--- index.ts
+++ index.ts
@@ -149,3 +149,45 @@ class Animator {
         }
     }
 }
+
+class HAFLNode {
+
+    state : State = new State()
+    prev : HAFLNode 
+    next : HAFLNode 
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new HAFLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawHAFLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+    
+    getNext(dir : number, cb : Function) : HAFLNode {
+        var curr : HAFLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
